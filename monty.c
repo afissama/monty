84d8973305886489f067cc98b46133d55319e7f8
afis <mahoutinafis@gmail.com>
2022-09-20 15:50:33 +0100

    stderr usage

--- monty.c
+++ monty.c
@@ -16,11 +16,16 @@ int main(int argc, char const *argv[])
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	stack = NULL;
 	fd = fopen(argv[1], "r");
+	if (fd == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	nbline = 0;
 	while (fgets(line, sizeof(line), fd))
 	{
